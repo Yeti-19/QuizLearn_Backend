885e62b5d329831d1345e997317671f413779868
Aditya Bhalla <142248663+Yeti-19@users.noreply.github.com>
2025-01-29 02:27:25 +0530

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class UserRegister(BaseModel):
+    username: str
+    email: str
+    password: str
+    phone_number: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+# UserResponse schema to return selected fields (e.g., excluding the password)
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    phone_number: str
+
+    class Config:
+        orm_mode = True  # Tells Pydantic to treat SQLAlchemy models as dictionaries
