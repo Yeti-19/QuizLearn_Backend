4ae1c31338586e60abe466903ab8607ab5d0725d
Aditya Bhalla <142248663+Yeti-19@users.noreply.github.com>
2025-01-29 07:18:33 +0530

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -16,7 +16,27 @@ class UserResponse(BaseModel):
     username: str
     email: str
     phone_number: str
+class UserProgressResponse(BaseModel):
+    username: str
+    tokens_earned: int
+    experience_gained: int
+    rank: int
+    level: int
+    badge: str  # Add badge field to the response
+
+class UpdateProgressRequest(BaseModel):
+    tokens_earned: int
+    experience_gained: int
 
     class Config:
         orm_mode = True  # Tells Pydantic to treat SQLAlchemy models as dictionaries
+
+class UserRankResponse(BaseModel):
+    username: str
+    experience_gained: int
+    rank: int
+
+    class Config:
+        orm_mode = True  # Tells Pydantic to treat SQLAlchemy models as dictionaries
+
         
